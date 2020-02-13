71eadbabfe08cf0744b261624cf6b022028dd49c
hong <1078476723@qq.com>
2020-02-13 12:04:46 +0800

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,31 @@
 import { Component } from '@angular/core';
-
+import {Observable} from 'rxjs/Observable';
+import  'rxjs/add/operator/map';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  title = 'aspp';
+  using(){
+    // 定义数据源
+    let onSubscribe = (observer)=>{
+      observer.next(1);
+      observer.next(2);
+      observer.next(3);
+    
+    }
+    // 定义数据源
+      const source$ = Observable.create(onSubscribe);
+    // 对每一个数据，进行操作
+      source$.map((item)=>{
+        return item +1;
+      }).subscribe((res)=>{console.log(res)});
+   }
+   constructor(){
+     this.using();
+   }
+
+
 }
